123aafe2b9872cacefa55676d40d71bf5ad697e5
Parvathaneni Bhaskar - bparva <Bhaskar.Parvathaneni@acxiom.com>
2014-08-15 20:14:57 -0700

    R-Prog assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+#
+# Example 3x3 invertible matrix
+#
+# m <- matrix(c(2,3,2,1,2,1,1,1,2), nrow=3,ncol=3)
+# solve(m) gives inverse of matrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
@@ -12,4 +26,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    
+    if (!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    
+    matData <- x$get()
+    inv <- solve(matData)
+    x$setInverse(inv)
+    inv
 }
