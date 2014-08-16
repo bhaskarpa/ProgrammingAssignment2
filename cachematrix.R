b08deac3bdb06b98e51adbcc77abf7d8edf9fc4e
Parvathaneni Bhaskar - bparva <Bhaskar.Parvathaneni@acxiom.com>
2014-08-15 20:39:39 -0700

    Update R-Prog Assign2

--- cachematrix.R
+++ cachematrix.R
@@ -9,15 +9,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    # Initialize variable storing the inverse
     inv <- NULL
+    #
+    # Define function to set the matrix data in special cached matrix object
+    #
     set <- function(y) {
         x <<- y
+        #
+        # Reset the matrix inverse when matrix data is updated.
+        # This step will ensure that the matrix inverse will be recomputed.
+        #
         inv <<- NULL
     }
     
+    #
+    # Define get function to return the matrix data
+    #
     get <- function() x
+    #
+    # Define function to set the matrix inverse in the cached variable "inv" defined
+    # in the parent environment.
+    #
     setInverse <- function(inverse) inv <<- inverse
+    #
+    # Define function to return the matrix inverse
+    #
     getInverse <- function() inv
+    #
+    # Special matrix object created using this function will return a list of functions
+    # that can be used to manipulate the specail matrix object.
+    #
     list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
@@ -26,15 +48,34 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    #
+    # Call the "getInverse" function on the special matrix object "x" passed as function arg
+    #
     inv <- x$getInverse()
-    
+    #
+    # Check if cached inverse value is returned by special matrix object
+    #
     if (!is.null(inv)) {
         message("getting cached inverse")
+        #
+        # Return cached inverse value of matrix
+        #
         return(inv)
     }
-    
+    #
+    # Fetch the matrix data from the special matrix object "x"
+    #
     matData <- x$get()
+    #
+    # Compute the matrix inverse
+    #
     inv <- solve(matData)
+    #
+    # Set the matrix inverse value in the spcial matrix object
+    #
     x$setInverse(inv)
+    #
+    # Return the matrix inverse
+    #
     inv
 }
